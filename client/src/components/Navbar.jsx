fdf99261c7ba93213cfe578f35d3d5b25615207c
Karanjot Singh <k.jsingh.12003@gmail.com>
2024-03-11 15:26:30 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className="toggle w-6 block md:hidden">
         <img src={toggle} />
       </div>
-        <p className="font-bold text-lg flex items-center gap-1">ChatGPT <span className="text-[#676767] ">3.5 </span><i className='text-[var(--text-sec)] text-[0.6rem] fa-solid fa-chevron-down'></i></p>   
+        <p className="font-bold text-lg flex items-center gap-1">ChatGPT <span className="text-[#676767] ">3.5 </span></p>   
         <p onClick={()=>navigator.clipboard.writeText(window.location.href)} className='border-[0.05px] border-[#bdbdbd] px-3 p-2 text-sm rounded-xl cursor-pointer hover:border-[#676767] '><i className="fa-solid fa-upload"></i></p>   
     </div>
   )
